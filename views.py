4ac68b59e6d363bea8986e7ede842686ad4a5a7c
Ekaterina <cheburashka1997@gmail.com>
2017-06-16 02:02:11 +0300

    Create views.py
    Разбиение периода на промежутки с интервалом, заданным пользователем. 
    Поиск новостей с тегами в эти промежутки, циклическая обработка, отправка в данных для отрисовки графиков

--- views.py
+++ views.py
@@ -3,14 +3,35 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from .forms import SearchForm
-
+   
 def search(request):
+    values = []
     if request.GET['tag'] != '':
-        return render(request, 'search_results.html', {'tag': request.GET['tag']})
-        # return HttpResponseRedirect('/search/')
+        tag = request.GET['tag']
+
+        start_date = request.GET['start_date']
+        end_date = request.GET['end_date']
+
+        step = int(request.GET['interval']) * 3600
+
+        start = int(time.mktime(datetime.datetime.strptime(start_date, "%Y-%m-%d").timetuple()))
+        end = int(time.mktime(datetime.datetime.strptime(end_date, "%Y-%m-%d").timetuple()))
+
+        while (start <= end - step):
+            time.sleep(5)
+            count = count_tag(tag, start, start + step)
+            print(count)
+            format_date = datetime.datetime.fromtimestamp(start).strftime("%b %d %Y %H:%M:%S")
+            print(format_date)
+            values.append([format_date, count])
+            start = start + step
+        return render(request, 'search_results.html', {'color': request.GET['color'],
+                                                       'tag': tag,
+                                                       'values': values})
+
     else:
         return HttpResponse('You submitted an empty form.')
-
+ 
 
 def search_form(request):
     if request.method == 'GET':
